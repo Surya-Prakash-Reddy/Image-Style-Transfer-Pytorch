5c26322136a71c3829c19ad27e73d716ddc0d630
katsunori waragai <katsunori.waragai@groove-x.com>
2023-06-15 17:17:59 +0900

    converted from notbeook

---
+++ style_transfer.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from google.colab import drive
+drive.mount('/content/drive/')
+
+
+# ls
+
+# In[ ]:
+
+
+get_ipython().system('pip install torch torchvision')
+
+
+# In[ ]:
+
+
+#Doing this because, sometimes we get an error 'module 'PIL.Image' has no attribute 'register_extensions' in Google Colab
+#uninstall the old one
+get_ipython().system('pip uninstall -y Pillow')
+# install the new one
+get_ipython().system('pip install Pillow==4.1.1')
+
+
+# In[ ]:
+
+
+#importing libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+import torch
+from torchvision import transforms, models
+
+
+# In[ ]:
+
+
+vgg = models.vgg19(pretrained=True).features
+
+for param in vgg.parameters():
+    param.requires_grad_(False)
+
+
+# In[ ]:
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Cuda Available: ', torch.cuda.is_available())
+vgg.to(device)
+
+
+# In[ ]:
+
+
+def load_image(path, max_size=400, shape=None):
+    image = Image.open(path).convert('RGB')
+    
+    if max(image.size) > max_size:
+        size = max_size
+    else:
+        size = max(image.size)
+    if shape is not None:
+        size = shape
+    
+    in_transform = transforms.Compose([
+        transforms.Resize(size),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+    ])
+    
+    image = in_transform(image)[:3,:,:].unsqueeze(0)
+    
+    return image
+    
+
+
+# In[ ]:
+
+
+#load content image
+content = load_image('drive/My Drive/Colab Notebooks/Deep Learning/PyTorch Scholarship/Style Transfer/assets/surya2.jpg').to(device)
+
+#load style image
+style = load_image('drive/My Drive/Colab Notebooks/Deep Learning/PyTorch Scholarship/Style Transfer/assets/oily_mcoilface.jpg', shape=content.shape[-2:]).to(device)
+
+
+# In[ ]:
+
+
+#function to convert image from it's normalised form to back to regular form
+def imconvert(tensor):
+    tensor = tensor.cpu().clone().detach()
+    tensor = tensor.numpy().squeeze()
+    tensor = tensor.transpose(1,2,0)
+    tensor = tensor * np.array((0.229, 0.224, 0.225)) + np.array((0.485, 0.456, 0.406))
+    tensor = tensor.clip(0,1)
+    return tensor
+
+
+# In[ ]:
+
+
+fig = plt.figure(figsize=(20,10))
+ax1 = fig.add_subplot(1,2,1, xticks=[], yticks=[])
+ax1.imshow(imconvert(content))
+ax2 = fig.add_subplot(1,2,2, xticks=[], yticks=[])
+ax2.imshow(imconvert(style))
+
+
+# In[ ]:
+
+
+#printing the vgg model
+vgg
+
+
+# In[ ]:
+
+
+#defining the function to get layers
+def get_features(image, model, layers=None):
+    if layers is None:
+        layers = {'0':'conv1_1','5':'conv2_1', '10':'conv3_1', '19':'conv4_1', '28':'conv5_1', '21':'conv4_2'}
+    
+    features = {}
+    for name, layer in model._modules.items():
+        image = layer(image)
+        if name in layers:
+            features[layers[name]]=image
+    
+    return features
+
+
+# In[ ]:
+
+
+#calculating the gram matrix
+def gram_matrix(tensor):
+    batch_size, depth, height, width = tensor.shape
+    
+    tensor = tensor.view(depth, -1)
+    tensor = torch.mm(tensor, tensor.t())
+    return tensor
+
+
+# In[ ]:
+
+
+style_features = get_features(style, vgg)
+content_features = get_features(content, vgg)
+
+style_grams = {layer: gram_matrix(style_features[layer]) for layer in style_features}
+
+#we could start with random image, but it would be good to start with content image
+target = content.clone().requires_grad_(True).to(device)
+
+
+# In[ ]:
+
+
+style_weights = {'conv1_1': 1.,
+                 'conv2_1': 0.8,
+                 'conv3_1': 0.5,
+                 'conv4_1': 0.3,
+                 'conv5_1': 0.1}
+
+
+content_weight = 1  # alpha
+style_weight = 5e6  # beta
+
+
+# In[ ]:
+
+
+optimizer = torch.optim.Adam([target], lr=0.003)
+
+steps = 2400
+print_every = 400
+
+for i in range(1,steps+1):
+    
+    target_features = get_features(target, vgg)
+    content_loss = torch.mean((content_features['conv4_2']-target_features['conv4_2'])**2)
+    
+    style_loss = 0
+    for layer in style_weights:
+        
+        target_feature = target_features[layer]
+        
+        _, d, h, w = target_feature.shape
+        
+        target_gram = gram_matrix(target_feature)
+        
+        style_gram = style_grams[layer]
+        
+        layer_style_loss = style_weights[layer]*torch.mean((target_gram - style_gram)**2)
+        
+        style_loss += layer_style_loss/ (d*h*w)
+    
+    total_loss = style_weight*style_loss + content_weight*content_loss
+    
+    optimizer.zero_grad()
+    total_loss.backward()
+    optimizer.step()
+    
+    if i%print_every==0:
+        print('Total Loss: ', total_loss.item())
+        plt.imshow(imconvert(target))
+
+
+# In[ ]:
+
+
+fig = plt.figure(figsize=(22,10))
+ax1 = fig.add_subplot(1,3,1, xticks=[], yticks=[])
+ax1.imshow(imconvert(content))
+ax2 = fig.add_subplot(1,3,2, xticks=[], yticks=[])
+ax2.imshow(imconvert(style))
+
+ax3 = fig.add_subplot(1,3,3, xticks=[], yticks=[])
+ax3.imshow(imconvert(target))
+
+
+# In[ ]:
+
+
+plt.imsave('drive/My Drive/Colab Notebooks/Deep Learning/PyTorch Scholarship/Style Transfer/assets/surya2_style.jpg', imconvert(target))
+
+
+# In[ ]:
+
+
+
+
